d74f03081acf7e237194e66ee87f6a35b1020b56
Mad Bernard <madeline.a.bernard@gmail.com>
2016-01-05 15:42:01 -0800

    css to make glyphs line up right; a bit more db/server setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
-  var express     = require('express'),
-      db    = require('pg');
+var express = require('express');
+var pg = require('pg');
 
 // export DATABASE_URL=postgres:///$(whoami)
 
 // db.connect('mongodb://localhost/shortly'); // connect to mongo database named shortly
+
+pg.connect(process.env.DATABASE_URL + '?ssl=true', function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 var app = express();
 
