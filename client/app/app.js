38d1375d895fc577786d060534617d419849c932
Mad Bernard <madeline.a.bernard@gmail.com>
2016-01-05 17:57:26 -0800

    Working syntax to get ng-class rolling and to get model and controller together

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,23 @@
-angular.module('specGlyph', ['ui.router']);
-
-specGlyph.controller('GlyphClasser', ['$scope', function($scope) {
+angular.module('specGlyph', ['ui.router'])
 
 
+.controller('GlyphClasser', ['$scope', '$rootScope', function($scope, $rootScope) {
+  $scope.foo = {
+    pipe: true,
+    bk: false
+  };
+
+
+  $scope.yesPipe = function(param) {
+  console.log($scope.nameBox, 'in yesPipe');
+  console.log(param, 'param');
+
+    if ($scope.nameBox) {
+      return true;
+    }
+    else return true;
+  };
 
 }]);
+
+// <p class="hi" ng-class="{pipe: yesPipe(), bk: yesBlock(), stack: yesStack(), line: yesLine()}">{{ nameBox.toLowerCase() }}</p>
