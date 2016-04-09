65a03ae0a22b3e98f068da53ce79961257313046
Mad Bernard <madeline.a.bernard@gmail.com>
2016-04-09 15:53:05 -0700

    Setting up for full flexbox for CSS

--- server/server.js
+++ server/server.js
@@ -5,16 +5,16 @@ var pg = require('pg');
 
 // db.connect('mongodb://localhost/shortly'); // connect to mongo database named shortly
 
-pg.connect(process.env.DATABASE_URL + '?ssl=true', function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
+// pg.connect(process.env.DATABASE_URL + '?ssl=true', function(err, client) {
+//   if (err) throw err;
+//   console.log('Connected to postgres! Getting schemas...');
 
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
+//   client
+//     .query('SELECT table_schema,table_name FROM information_schema.tables;')
+//     .on('row', function(row) {
+//       console.log(JSON.stringify(row));
+//     });
+// });
 
 var app = express();
 
